b19f1dda6837f5bd96d99577da757884f7489d3f
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-30 08:47:27 -0300

    Add leitura do txt com as palavras no viewController

--- 100DaysOfSwift-Project5/ViewController.swift
+++ 100DaysOfSwift-Project5/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") else {
+            print("No file found")
+            return
+        }
+        
+        // Transforma o arquivo txt em uma string para poder ler depois
+        guard let startWords = try? String(contentsOf: startWordsURL) else {
+            print("Can't transform file in string")
+            return
+        }
+        
+        // Cada linha é separada por um \n na string, entao separa as palavras por isso
+        allWords = startWords.components(separatedBy: "\n") // Transforma string em array
+        
+        if allWords.isEmpty {
+            allWords = ["Potato"]
+        }
     }
 
 
