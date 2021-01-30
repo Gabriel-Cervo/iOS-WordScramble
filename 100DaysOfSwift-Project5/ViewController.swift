890789f7611859f820e19d64cc33e0f5828bd1d1
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-30 09:21:51 -0300

    Add alertController e textField para usuario poder digitar o guess da palavra

--- 100DaysOfSwift-Project5/ViewController.swift
+++ 100DaysOfSwift-Project5/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
         guard let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") else {
             print("No file found")
             return
@@ -41,6 +43,25 @@ class ViewController: UITableViewController {
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
+    
+    @objc func promptForAnswer() {
+        let alertController = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        alertController.addTextField()
+        
+        // Como o swift captura todos objetos usados na closure, definir como weak o proprio ViewController (self), pois chama o metodo submit dentro da closure, caso contrario acontecera um strong reference cycle
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak alertController] _ in
+            guard let answer = alertController?.textFields?[0].text else { return }
+            
+            self?.submit(answer) // Sempre usar self quando chamar algo de fora da closure mas mesma classe
+            // Sempre cuidar os self nas closures, podem causar reference cycle...
+        }
+        
+        alertController.addAction(submitAction)
+        
+        present(alertController, animated: true)
+    }
+    
+    func submit(_ answer: String) {}
     
     //MARK: TableView methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
